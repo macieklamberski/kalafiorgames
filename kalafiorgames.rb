5df3adabcd0260e8bc950856420c5fb1e634cac4
Maciej Lamberski <maciej@lamberski.com>
2016-04-01 18:34:49 +0200

    Remove comments

--- kalafiorgames.rb
+++ kalafiorgames.rb
@@ -1,11 +1,7 @@
-# -- Configuration -------------------------------------------------------------
-
 APP_ROOT = File.expand_path(File.dirname(__FILE__))
 
 DataMapper::setup(:default, "sqlite3://#{APP_ROOT}/db.sqlite3")
 DataMapper::Model.raise_on_save_failure = true if development?
-
-# -- Models --------------------------------------------------------------------
 
 class Game
   include DataMapper::Resource
@@ -22,29 +18,22 @@ class File
   property :downloads, Integer, :default => 0
 end
 
-# -- Helpers -------------------------------------------------------------------
-
 helpers do
   def selected_if(page)
     ' class="selected"' if @page == page
   end
 end
 
-# -- Actions -------------------------------------------------------------------
-
-# Index
 get '/' do
   @page = 'index'
   erb 'pages/index'.to_sym
 end
 
-# About
 get '/about' do
   @page = 'about'
   erb 'pages/about'.to_sym
 end
 
-# Files download
 get '/download/:slug' do
   file = File.first(:slug => params[:slug])
   if !file.nil?
@@ -56,7 +45,6 @@ get '/download/:slug' do
   end
 end
 
-# Game details
 get '/:slug' do
   @page = 'game'
   @game = Game.first(:slug => params[:slug])
@@ -67,7 +55,6 @@ get '/:slug' do
   end
 end
 
-# Error 404
 not_found do
   @page = 'error'
   erb 'pages/404'.to_sym
