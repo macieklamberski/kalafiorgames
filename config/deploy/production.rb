c8e0a827a03ee142eede50a498582b2451e751c4
Maciej Lamberski <maciej@lamberski.com>
2016-04-02 00:22:31 +0200

    Replace built-in restart mechanism with custom one

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :branch, 'master'
 set :deploy_to, ENV['DEPLOY_PATH']
 set :default_env, { rvm_bin_path: '~/.rvm/bin' }
-server ENV['DEPLOY_HOST'], port: ENV['DEPLOY_PORT'], user: ENV['DEPLOY_USER'], roles: %{all}
+server ENV['DEPLOY_HOST'], port: ENV['DEPLOY_PORT'], user: ENV['DEPLOY_USER'], roles: %{app}
