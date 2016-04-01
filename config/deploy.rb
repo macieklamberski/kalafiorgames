edfb01b448938eebe7629f493a8a6a8ae0446756
Maciej Lamberski <maciej@lamberski.com>
2016-04-01 23:54:24 +0200

    Add ZIPs with game files to shared directories

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,3 +4,4 @@ Dotenv.load
 set :application, 'kalafiorgames'
 set :repo_url, 'https://github.com/lamberski/kalafiorgames.git'
 set :linked_files, ['.env', ENV['DB_PATH']]
+set :linked_dirs, ['public/files', 'tmp']
