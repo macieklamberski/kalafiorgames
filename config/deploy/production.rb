b873c4af544319e0177c9a27a34fa47bfc6b52c4
Maciej Lamberski <maciej@lamberski.com>
2016-05-31 21:45:32 +0200

    Remove old version of the site

--- config/deploy/production.rb
+++
@@ -1,4 +0,0 @@
-set :branch, 'master'
-set :deploy_to, ENV['DEPLOY_PATH']
-set :default_env, { rvm_bin_path: '~/.rvm/bin' }
-server ENV['DEPLOY_HOST'], port: ENV['DEPLOY_PORT'], user: ENV['DEPLOY_USER'], roles: %{app}
