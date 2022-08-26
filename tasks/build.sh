2af8f725562979312848c4861fd393a35af4a6dd
Maciej Lamberski <maciej@lamberski.com>
2022-08-26 13:52:37 +0200

    Update build task

--- tasks/build.sh
+++ tasks/build.sh
@@ -4,19 +4,17 @@ TEMPLATES_DIR=templates/pages
 
 # Clear build directory.
 rm -rf $BUILD_DIR
-mkdir $BUILD_DIR
-
-# Go through page files and generate static HTML version.
-for f in $TEMPLATES_DIR/*.twig
-do
-   a=${f/$TEMPLATES_DIR\//}
-   b=${a/.twig/}
-
-   echo $(php index.php --request-uri=$b) >> $BUILD_DIR/$b.html
-done
 
 # Copy all static files to build directory.
-cp -r $STATIC_DIR/* build
+cp -rT $STATIC_DIR $BUILD_DIR
+
+# Go through page templates and generate static HTML versions.
+for f in $TEMPLATES_DIR/*.twig
+do
+  a=${f/$TEMPLATES_DIR\//}
+  b=${a/.twig/}
+  echo $(php index.php --request-uri=$b) >> $BUILD_DIR/$b.html
+done
 
 # Build stylesheet file using Sass.
-npx sass styles/main.scss build/styles/main.css --style compressed --no-source-map
+npx sass styles/main.scss $BUILD_DIR/styles/main.css --style compressed --no-source-map
